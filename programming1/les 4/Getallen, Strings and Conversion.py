7c1c9cae3638f8eb4aaac1f27bd416955b60a128
MartNotermans <43001231+MartNotermans@users.noreply.github.com>
2018-09-13 10:02:29 +0200

    opdrachten les 4

---
+++ programming1/les 4/Getallen, Strings and Conversion.py
@@ -0,0 +1,6 @@
+cijferICOR=6
+cijferPROG=8
+cijferCSN=7
+gemiddelde = (cijferCSN+cijferCSN+cijferPROG) /3
+beloning = 30*gemiddelde
+print ('beloning:', beloning)
---
+++ programming1/les 4/vote.py
@@ -0,0 +1,7 @@
+name = input ('wat is je naam: ')
+agestring = input('wat is je leeftijd:')
+age = int(agestring)
+if (age >=18):
+    print ('je mag stemmen')
+else:
+    print ('je mag niet stemmen')
