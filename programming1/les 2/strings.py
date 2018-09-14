cd2c73222588174e4106626138ee4464f0b35802
MartNotermans <43001231+MartNotermans@users.noreply.github.com>
2018-09-14 14:59:19 +0200

    opdracht 2, strings

---
+++ programming1/les 2/strings.py
@@ -0,0 +1,7 @@
+print (len('Supercalifragilisticexpialidocious'))
+print ('ice' in 'Supercalifragilisticexpialidocious' )
+print (len('Antidisestablishmentarianism') > len('Honorificabilitudinitatibus'))
+lst = ['Berlioz', 'Borodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein']
+sorted (lst)
+print (lst[0])
+print (lst [-1])
