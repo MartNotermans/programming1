bac64f9485d07cb5abe932f3fff2aaa9b3a9d3a6
MartNotermans <43001231+MartNotermans@users.noreply.github.com>
2018-09-27 14:14:18 +0200

    werkt nog niet perfect, weet niet waarom

--- les 6/Final Assignment - NS-functies.py
+++ les 6/Final Assignment - NS-functies.py
@@ -1,19 +1,34 @@
 def standaardprijs(afstandKM):
-    if afstandKM < 50 and afstandKM > 0:
+    if 50 > afstandKM > 0:
         prijs = afstandKM*0,80
-    if afstandKM > 50:
+    elif afstandKM > 50:
         prijs = 15+afstandKM*0,60
-    if afstandKM < 0:
+    elif afstandKM < 0:
         prijs = 0
-def ritprijs (leeftijd, weekendrit, afstandKM):
-    if weekendrit is 'weekendrit':
-        return True
-    else:
-        return False
+    return prijs
+def bepaal_weekend(dag):
+    return len(dag) > 0 and dag.lower()[0] == 'z'
 
-if  weekendrit return == True:
-        leeftijd < 12 or leeftijd >= 65:
-        prijs2 = prijs * 0,70
+
+afstandKM = eval(input('hoeveel KM ga je reizen? '))
+dag = input('welke dag ga je reizen? ')
+leeftijd = int(input('wat is uw leeftijd? '))
+
+weekendrit = bepaal_weekend(dag)
+prijs = standaardprijs(afstandKM)
+
+def ritprijs (leeftijd, weekendrit, afstandKM):
+    if leeftijd < 12 or leeftijd >= 65:
+        if weekendrit:
+            prijs2 = prijs * 0,70
+        else:
+            prijs2 = prijs * 0,65
     else:
-        leeftijd < 12 or leeftijd >= 65:
-        prijs2 = prijs * 0,65
+        if weekendrit:
+            prijs2 = prijs * 0,60
+        else:
+            prijs2 = prijs
+    return prijs2
+prijs2 = ritprijs(leeftijd,weekendrit,afstandKM)
+print('prijs voor korting: ', prijs)
+str(print('prijs2: ',prijs2))
