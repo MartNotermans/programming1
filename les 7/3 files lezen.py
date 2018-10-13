d4fdd1b6d0fd4d601de37d7aea8263c0bbbc07c1
MartNotermans <43001231+MartNotermans@users.noreply.github.com>
2018-10-13 16:38:34 +0200

    huiswerk

---
+++ les 7/3 files lezen.py
@@ -0,0 +1,12 @@
+filename = "kaartnummers"
+file = open(filename, "r")
+number_of_lines = 0
+mylist = []
+for line in file:
+    number_of_lines += 1
+    lst = line.split(",")
+    kaartnummer = lst[0]
+    naam = lst[1]
+    mylist.append(lst[0])
+print('deze file telt',number_of_lines,'regels')
+print('het grootste kaartnummer is:',max(mylist),'en staat op regel',mylist.index(max(mylist)))
