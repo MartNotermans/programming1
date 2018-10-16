4e402c014b3c4f1f566a630d27e8f3fdcd0acc4e
MartNotermans <43001231+MartNotermans@users.noreply.github.com>
2018-10-16 13:37:47 +0200

    hallo

--- les 7/3 files lezen.py
+++ les 7/3 files lezen.py
@@ -8,5 +8,6 @@ for line in file:
     kaartnummer = lst[0]
     naam = lst[1]
     mylist.append(lst[0])
+regel_nummer = mylist.index(max(mylist)) + 1
 print('deze file telt',number_of_lines,'regels')
-print('het grootste kaartnummer is:',max(mylist),'en staat op regel',mylist.index(max(mylist)))
+print('het grootste kaartnummer is:',max(mylist),'en staat op regel',regel_nummer)
