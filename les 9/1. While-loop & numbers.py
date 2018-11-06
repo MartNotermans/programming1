bfca51ab3c781b2aac31c30826dbe80082cf441e
MartNotermans <43001231+MartNotermans@users.noreply.github.com>
2018-11-06 14:16:45 +0100

    hallo

--- les 9/1. While-loop & numbers.py
+++ les 9/1. While-loop & numbers.py
@@ -1,2 +1,8 @@
-while userinput =! 0
-    userinput = input('geef een getal:')
+lst = []
+while True:
+    if 0 not in lst:
+        userinput = eval(input('geef een getal:'))
+        lst.append(userinput)
+    else:
+        print('er zijn', len(lst), 'getallen ingevoerd, de som is:', sum(lst))
+        break
